82b408ade2e10a9711f80074e8b1bdd9a312e0aa
alex-v08 <alex.velasquez08@outlook.com>
2024-07-21 15:20:38 -0300

    fix: minor fix remove method iUserserivce

--- src/main/java/com/spaceplanner/booking/service/IUserService.java
+++ src/main/java/com/spaceplanner/booking/service/IUserService.java
@@ -6,18 +6,7 @@ import jakarta.validation.Valid;
 
 import java.util.List;
 
-public interface IUserService {
-
-
-    public List<User> findAllUsers();
-
-    public User saveUser(User user);
-
-    public User updateUser(User user);
-
-    public User findUserById(Long id);
-
-    public void deleteUser(Long id);
+public interface  IUserService {
 
 
 
