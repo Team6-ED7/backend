6c36d057aea925f3a421b1565993e7a8ba47266c
Alex Velasquez <alex.velasquez08@outlook.com>
2024-07-23 06:44:24 -0300

    feat: Generate space repository

--- src/main/java/com/spaceplanner/booking/space/controller/SpaceController.java
+++ src/main/java/com/spaceplanner/booking/space/controller/SpaceController.java
@@ -1,4 +1,26 @@
 package com.spaceplanner.booking.space.controller;
 
+import com.spaceplanner.booking.space.entity.Space;
+import com.spaceplanner.booking.space.entity.dto.SpaceDto;
+import com.spaceplanner.booking.space.service.ISpaceService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/spaces")
 public class SpaceController {
+
+    @Autowired
+    private ISpaceService spaceService;
+
+    @PostMapping("/register")
+    public ResponseEntity<Space> registerSpace(@Valid @RequestBody SpaceDto spaceDto) {
+        return new ResponseEntity<>(spaceService.registerSpace(spaceDto), HttpStatus.CREATED);
+    }
 }
--- src/main/java/com/spaceplanner/booking/user/controller/UserController.java
+++ src/main/java/com/spaceplanner/booking/user/controller/UserController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/users")
-public class userController {
+public class UserController {
 
     @Autowired
     private IUserService userService;
