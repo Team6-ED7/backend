3a8b1cc68dc876efbaf2b76490f7160687223605
Abraham Gutierrez <appbraham.dev@gmail.com>
2024-07-24 13:52:26 -0500

    feat: add TypeSpace repository

---
+++ src/main/java/com/spaceplanner/booking/typespace/repository/ITypeSpaceRepository.java
@@ -0,0 +1,11 @@
+package com.spaceplanner.booking.typespace.repository;
+
+import com.spaceplanner.booking.typespace.entity.TypeSpace;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ITypeSpaceRepository extends JpaRepository<TypeSpace, Long> {
+
+    Boolean existsTypeSpaceByName(String name);
+}
